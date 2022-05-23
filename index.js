7d686f7550b536cefec9bb3594f51520c03bc0ed
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-05-23 11:12:08 -0300

    Feat: Primeira encriptação

---
+++ index.js
@@ -0,0 +1,33 @@
+const firstEncryption = (wordToBeEncrypted) => {
+  const wordArray = wordToBeEncrypted.split('');
+  const vinNumberLength = wordArray.length;
+  const hash = wordArray.map((_charactere, index) => {
+    const charactereCode = wordToBeEncrypted.charCodeAt(index);
+    const encryptedCharacterCode = charactereCode + vinNumberLength;
+    return String.fromCharCode(encryptedCharacterCode);
+  });
+  return hash;
+};
+
+const thirdDecryption = (wordToBeDecrypted) => {
+  const wordArray = wordToBeDecrypted.split('');
+  const vinNumberLength = wordArray.length;
+  const hash = wordArray.map((_charactere, index) => {
+    const charactereCode = wordToBeDecrypted.charCodeAt(index);
+    const encryptedCharacterCode = charactereCode - vinNumberLength;
+    return String.fromCharCode(encryptedCharacterCode);
+  });
+  return hash;
+};
+
+const crypto = (word) => {
+  let wordEncrypted = word;
+  wordEncrypted = firstEncryption(wordEncrypted);
+  return wordEncrypted;
+};
+
+const decrypto = (hash) => {
+  let wordDecrypted = hash;
+  wordDecrypted = thirdDecryption(wordDecrypted);
+  return wordDecrypted;
+};
